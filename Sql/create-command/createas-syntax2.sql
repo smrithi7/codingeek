d086938dae79c3a019f34cc5c03161ac79274cdc
smrithi7 <k.srimathi7@gmail.com>
2017-08-16 21:26:31 +0530

    Update createas-syntax2.sql

--- Sql/create-command/createas-syntax2.sql
+++ Sql/create-command/createas-syntax2.sql
@@ -1,4 +1,15 @@
-CREATE TABLE new_table_name AS
-    SELECT column1, column2,...
-    FROM existing_table
-    WHERE ...;
+-- SYNTAX
+
+CREATE TABLE new_table_name
+  AS (
+	SELECT column_name1,column_name2...
+      FROM old_table_name
+      WHERE condition
+);
+
+-- EXAMPLE
+
+CREATE TABLE Average
+  AS (SELECT id,name
+      FROM Student
+      WHERE Total < 170);

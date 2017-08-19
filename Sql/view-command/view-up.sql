f94b7bfb854e409ff1986b4aab39153deabd6cc6
smrithi7 <k.srimathi7@gmail.com>
2017-08-19 08:06:27 +0530

    Update view-up.sql

--- Sql/view-command/view-up.sql
+++ Sql/view-command/view-up.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW view_name AS
-SELECT t1.column_name1, t2.column_name2, t2.column_name3 etc
-FROM table_name1 AS t1, table_name2 AS t2...
+SELECT v1.column_name1, v2.column_name2, v2.column_name3 etc
+FROM table_name1 AS v1, table_name2 AS v2...
 WHERE  [condition];
